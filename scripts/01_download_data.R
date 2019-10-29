2ce9e29c942c287f3941c36db5208fe9836b714f
Kory Melton <kmelton@dons.usfca.edu>
2019-10-29 00:13:32 +0000

    Add script to download data files Neon

---
+++ scripts/01_download_data.R
@@ -0,0 +1,28 @@
+#############################################################
+# Script to download raw NEON data files
+# This is the first step in a processing pipeline
+# Kory Melton
+# October 23, 2019
+# kmelton@dons.usfca.edu
+#############################################################
+
+source("scripts/downloadRawSequenceData.R")
+
+# download all of the raw sequence 16S data for the
+# UNDE site and put it in the /data directory
+
+rawdata_metadata <- downloadRawSequenceData(
+	dpid = "DP1.10108.001",
+	sites = "UNDE",
+	targetGene = "16S",
+	startYrMo = NA,
+	endYrMo = NA,
+	checkFileSize = FALSE,
+	outdir = "/data")
+
+# write out the metadata for all the samples to a
+# csv file
+
+write.csv(
+	rawdata_metadata,
+	"/data/rawdata_metadata.csv")
