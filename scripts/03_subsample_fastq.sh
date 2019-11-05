320cdf28f7d886699a01d844e48a1e1a57e91725
Kory Melton <kmelton@dons.usfca.edu>
2019-11-05 02:03:11 +0000

    Change subsampling percent from 1% to 0.5% of files

--- scripts/03_subsample_fastq.sh
+++ scripts/03_subsample_fastq.sh
@@ -1,4 +1,4 @@
 # subsample to 1% of the total number of reads in the file.fastq
 # this will speed up the blast searching SIGNIFICANTLY and is a key step
 # needs to be turned into a for loop to work on all files
-cat file.fastq | paste - - - - | awk 'BEGIN{srand(1234)}{if(rand() < 0.01) print $0}' | tr '\t' '\n' > out.fastq
+cat file.fastq | paste - - - - | awk 'BEGIN{srand(1234)}{if(rand() < 0.005) print $0}' | tr '\t' '\n' > out.fastq
