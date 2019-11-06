00baa56bb5f5514ff8969a905a7b89d8f9139bad
Kory Melton <kmelton@dons.usfca.edu>
2019-11-06 04:41:47 +0000

    Edited 04_trimmomatic.sh to trim a specific file

--- scripts/04_trimmomatic.sh
+++ scripts/04_trimmomatic.sh
@@ -1,4 +1,6 @@
+#!/bin/bash
+
 # run trimmomatic to throw out bad sequences, trim when quality gets low, or if
 # sequences are too short you will need to turn this into a for loop to process
 # all of your SUBSAMPLED files
-TrimmomaticSE -threads 4 -phred33 data/raw_data/YOURFILENAME.fastq data/trimmed/$(basename -s .fastq YOURFILENAME.fastq).trim.fastq LEADING:5 TRAILING:5 SLIDINGWINDOW:8:25 MINLEN:200
+TrimmomaticSE -threads 4 -phred33 /data/subsampled-fastq/BMI_Plate51WellE1_16S_R1.subsampled.fastq /data/trimmed-fastq/$(basename -s .fastq /data/subsampled-fastq/BMI_Plate51WellE1_16S_R1.subsampled.fastq).trim.fastq LEADING:5 TRAILING:5 SLIDINGWINDOW:8:25 MINLEN:200
