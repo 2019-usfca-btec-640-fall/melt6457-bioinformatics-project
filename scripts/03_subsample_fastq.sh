ba4df9f83c17a1f75a536a479ebaa2c6e8246274
Kory Melton <kmelton@dons.usfca.edu>
2019-11-06 04:38:50 +0000

    Wrote script to subsample all of the raw-fastq files

--- scripts/03_subsample_fastq.sh
+++ scripts/03_subsample_fastq.sh
@@ -3,4 +3,12 @@
 # subsample to 1% of the total number of reads in the file.fastq
 # this will speed up the blast searching SIGNIFICANTLY and is a key step
 # needs to be turned into a for loop to work on all files
-cat /data/raw-fastq-files/BMI_Plate51WellE1_16S_R1.fastq | paste - - - - | awk 'BEGIN{srand(1234)}{if(rand() < 0.005) print $0}' | tr '\t' '\n' > /data/subsampled-fastq/BMI_Plate51WellE1_16S_R1.subsampled.fastq
+
+for file in /data/raw-fastq-files/*.fastq
+do
+	# use echo to print out progress of script
+	echo $file
+
+	# randomly subsampled .5% of the fastq files
+	cat $file | paste - - - - | awk 'BEGIN{srand(1234)}{if(rand() < 0.005) print $0}' | tr '\t' '\n' > /data/subsampled-fastq/$(basename -s .fastq $file).subsampled.fastq
+done
