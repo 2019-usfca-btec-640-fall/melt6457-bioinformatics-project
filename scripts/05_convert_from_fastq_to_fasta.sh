e1a71ecb2e43850fabf66c7b3f9617fcf9dcf682
Kory Melton <kmelton@dons.usfca.edu>
2019-11-06 05:41:13 +0000

    Write for loop to convert fastq files to fasta files

--- scripts/05_convert_from_fastq_to_fasta.sh
+++ scripts/05_convert_from_fastq_to_fasta.sh
@@ -1,5 +1,12 @@
+#!/bin/bash
+
 # convert to fasta for BLAST
 # you need to modify this to save the converted fasta file to a file
 # instead of printing to the screen
 # you'll need to turn this into a for loop too
-bioawk -c fastx '{print ">"$name"\n"$seq}' data/trimmed/filename.trim.fastq
+
+for file in /data/trimmed-fastq/*.fastq
+do
+	echo $file
+	bioawk -c fastx '{print ">"$name"\n"$seq}' $file > /data/trimmed-fasta/$(basename -s .fastq $file).fasta
+done
