916ad05c0fa3110a54c05d873e8aa4ab978186ae
Kory Melton <kmelton@dons.usfca.edu>
2019-11-10 01:01:48 +0000

    Edit fastq_counts.sh to count information on fasta and fastq files

--- scripts/fastq_counts.sh
+++ scripts/fastq_counts.sh
@@ -10,3 +10,15 @@ done
 echo "\nNumber of fastq files: "
 ls *.fastq | wc -l
 echo "\n"
+
+cd /data/subsampled-fastq
+
+echo "\n Samples per File"
+for file in /data/subsampled-fastq/*.fastq
+do
+	grep -c "^+$" $file
+done
+
+echo "\nNumer of subsampled files: "
+ls *.fastq | wc -l
+echo "\n"
