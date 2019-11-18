739f0146d30b24b39ebacdb031c16857864778d0
Kory Melton <kmelton@dons.usfca.edu>
2019-11-18 20:32:30 +0000

    Summarize blast results using uniq, cut, sort, and grep

--- scripts/07_summarize_blast_results.sh
+++ scripts/07_summarize_blast_results.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
 
 #######################################################################
+# Summarize Blast Results using multiple analyses:
+#	-Overview: get the counts for all unique blast matches
+#	-TopMatch: get the top match for each file with query cover 100
 #
-# use cut, sort, and uniq -c to help you summarize the results from the
-# BLAST search.
-#
-#
-#
-#
+# Author: Kory Melton
+# November 17, 2019
+# kmelton@dons.usfca.edu
 #######################################################################
 
 # get an overview of the species found in BLAST
 overviewFile=output/overview.txt
-cut -d, -f1 < output/blast_results.csv | uniq -c | sort | head -10 > "$overviewFile"
+cut -d, -f1 < output/blast_results.csv | uniq -c | sort | head -30 > "$overviewFile"
 
 # get the top match for each file
 
 # set the topMatchFile and create it
 topMatchFile=output/topMatch.txt
+rm output/topMatch.txt # remove previous iterations so you get a clean file
 touch output/topMatch.txt
 
 # use a for loop to go through each file
 for file in output/blast_results/*.csv
 do
-	cut -d, -f1 < "$file" | uniq -c | sort | cut -d' ' -f2,3 | head -1 >> "$topMatchFile"
+	grep "100.0" < "$file" | cut -d, -f1 | uniq -c | sort | awk '{$1=""; print}' | head -1 >> "$topMatchFile"
 done
 
 # get a count for how the unique top matches
